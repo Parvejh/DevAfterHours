f40cf32251852ceeadb46b19e7deee49ef8ee40f
Parvejh <101707909+Parvejh@users.noreply.github.com>
2026-08-13 15:12:42 +0530

    Basic Directory Setup complete

---
+++ devAfterHours-backend/server.js
@@ -0,0 +1,11 @@
+const http = require('http');
+const PORT = 8000;
+
+
+
+const server = http.createServer();
+
+
+server.listen(PORT,()=>{
+    console.log(`Server is running on PORT : ${PORT}`);
+})
