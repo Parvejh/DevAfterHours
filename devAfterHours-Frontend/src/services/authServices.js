b10f2bebb199d24bf70c98dbb34496df58e54ef4
Parvejh <101707909+Parvejh@users.noreply.github.com>
2026-08-27 18:24:45 +0530

    Corrected login issue

--- devAfterHours-Frontend/src/services/authServices.js
+++ devAfterHours-Frontend/src/services/authServices.js
@@ -5,7 +5,7 @@ const API_URL = import.meta.env.VITE_API_URL;
 export const loginUser = async (credentials)=>{
 
     const response = await axios.post(
-        `${API_URL}/login`,
+        `${API_URL}/auth/login`,
         credentials
     );
 
